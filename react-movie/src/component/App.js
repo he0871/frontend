c97a54701a4f04aff027064abc8370f4eaf348ed
Mark <jingyuan0871@outlook.com>
2020-04-13 22:09:29 -0400

    initialize

---
+++ react-movie/src/component/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header'
+import Products from './Products'
+class App extends React.Component{
+  render(){
+    return(
+      <div className="main">
+        <Header nickname="admin"/>
+        <Products/>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ react-movie/src/component/Products.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Products extends React.Component{
+  render(){
+    return(
+      <div className="products">
+        <p></p>
+      </div>
+    )
+  }
+}
+
+export default Products;
